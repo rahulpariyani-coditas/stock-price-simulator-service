8706b73bdcac30b99004319d4787a8847016ff10
Rahul Pariyani <rahul.pariyani@coditas.com>
2021-08-06 13:56:20 +0530

    feat: added mui table

---
+++ src/views/StocksListing/constants.js
@@ -0,0 +1,29 @@
+export const metadata = {
+  table: {
+    colDefs: [
+      {
+        label: "Name",
+        name: "name",
+      },
+      {
+        label: "Price (USD)",
+        name: "price",
+      },
+      {
+        label: "Quantity",
+        name: "quantity",
+      },
+      {
+        label: "Time",
+        name: "time",
+      },
+    ],
+    colFields: [
+      ["AALP", " 120", "-", "one second ago"],
+      ["GOOGL", " 2550", "100", "now"],
+      ["XYZ", " 2550", "-", "now"],
+      ["aBC", " 2550", "189", "now"],
+      ["MS", " 2550", "-", "now"],
+    ],
+  },
+};
--- src/views/StocksListing/index.js
+++ src/views/StocksListing/index.js
@@ -1,9 +1,15 @@
 import React from "react";
+import CustomizedTables from "../../components/CustomizedTables";
+import { metadata } from "./constants";
+import "./StocksListing.css";
 
 const StocksListing = () => {
   return (
     <div>
-      <h1>Stocks Listing Screen</h1>
+      <h1 align="center">Stocks Listing Screen</h1>
+      <section className="tableWrapper">
+        <CustomizedTables {...metadata.table} />
+      </section>
     </div>
   );
 };
